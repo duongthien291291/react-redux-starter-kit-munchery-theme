4d254abd2421b004ba8efe0012f734146caeae01
duongthien291291 <duongthien291291@gmail.com>
2017-06-30 13:05:21 +0700

    run success

---
+++ src/app/routes/Home/containers/HomeContainer.jsx
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux'
+
+import Home from '../components/Home'
+
+const mapDispatchToProps = {
+
+}
+
+const mapStateToProps = (state) => ({
+
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
